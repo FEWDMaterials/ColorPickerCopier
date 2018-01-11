3178bd9d3538586e1d51781591570de876b4e656
Mottaqui Karim <taqqui.karim@gmail.com>
2018-01-10 22:03:32 -0500

    adding notes

--- JS/main.js
+++ JS/main.js
@@ -1,64 +1,52 @@
-/*
-Click to generate random color.
-It will copy color rgb value and display the color, changing color of text based on color value.
-It will display up to 20 previous colors that were generated.
-Using the input field you can specify a color number previously generated
-to copy and then click copy color and it will copy the color rgb value you chose.
-
-USEFUL FOR CREATING A RANDOM COLOR AND COPYING COLOR RGB VALUE.
-*/
-
-
 const rand = ( s, e ) => {
     const random = s + Math.floor( Math.random() * (e-s+1) );
     return random;
 }
 
-const colorHolder = document.querySelector('.js-color-holder');
-const squareHolder = document.querySelector('.js-square-holder');
-const colorName = document.querySelector('.js-color-name');
-const colorHistoryView = document.querySelector('.js-color-history')
-const button = document.querySelector('.js-copy-button')
-const copyNumberValue = document.querySelector('.js-color-number-input')
-const counter = document.querySelector('.js-count');
-const total = document.querySelector('.js-total');
+const $colorHolder = $('.js-color-holder');
+const $squareHolder = $('.js-square-holder');
+const $counter = $('.js-count');
+const $total = $('.js-total');
+
+let colorHistory = [];
 const MAX_ALLOWED_COLORS = 5;
+$total.html(MAX_ALLOWED_COLORS);
 
-total.innerHTML = MAX_ALLOWED_COLORS;
 
-let colorHistory = []
-
-const onClick = event => {
+const onClick = () => {
     const R = rand(0,255);
     const G = rand(0,255);
     const B = rand(0,255);
     const randomColor = `rgb(${R},${G},${B})`
 
-    colorHolder.style.backgroundColor = randomColor;
-    squareHolder.innerHTML += `<div class="square" style="background-color: ${randomColor}"></div>`
+    $colorHolder.css('background-color', randomColor);
+    $squareHolder.html($squareHolder.html() + `<div class="square" style="background-color: ${randomColor}"></div>`);
 
     if (R < 150 || G < 150 || B < 150) {
-        colorHolder.style.color = "white";    
+        $colorHolder.css('color', 'white');
     }
     else {
-        colorHolder.style.color = "black";
+        $colorHolder.css('color', 'black');
     }
 
-    colorHolder.innerHTML = randomColor;
+    $colorHolder.html(randomColor);
     colorHistory.push(randomColor);
-    colorHistoryView.value = randomColor;
-
 
     if (colorHistory.length > MAX_ALLOWED_COLORS) {
-        squareHolder.innerHTML = ''
-        colorHolder.style.backgroundColor = null
-        colorHolder.innerHTML = 'Click to Generate Color'
-        colorHolder.style.color = "white";
+        $squareHolder.html('');
+        $colorHolder
+            .css({
+                'background-color': 'black',
+                'color': 'white'
+            })
+            .html('Click to Generate Color');
+
         colorHistory = []
-        colorHistoryView.value = colorHistory
     }
 
-    counter.innerHTML = colorHistory.length;
+    $counter.html(colorHistory.length);
 }
 
-colorHolder.addEventListener('click', onClick);
+// $colorHolder.on('click', onClick)
+$colorHolder.click(onClick)
+
